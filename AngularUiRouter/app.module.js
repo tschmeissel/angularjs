b065869702200139b72a1b180fe0926c49d075ea
Zaphod Beblerox <zaphod.beblerox@github.com>
2015-12-22 10:09:22 +0100

    single anonymous view added

--- AngularUiRouter/app.module.js
+++ AngularUiRouter/app.module.js
@@ -1,5 +1,28 @@
 (function() {
 	'use strict'
 	
-	angular.module('myApp', []);
+	var app = angular.module('myApp', ['ui.router']);
+	
+	app.config(function($logProvider) {
+		 $logProvider.debugEnabled(true);
+	});
+	
+	app.config(function($stateProvider, $urlRouterProvider) {
+		
+		$urlRouterProvider.otherwise('/home');
+		
+		$stateProvider
+        
+			// HOME STATES AND NESTED VIEWS ========================================
+			.state('home', {
+				url: '/home',
+				templateUrl: 'partial-home.html'
+			})
+			
+			// ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+			.state('about', {
+				// we'll get to this in a bit       
+		});		
+	});
+	
 })();
