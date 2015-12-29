a62a2cd010129df2b89140faa5c93dd3c884cf7f
Zaphod Beblerox <zaphod.beblerox@github.com>
2015-12-29 16:33:27 +0100

    another helpful comment

--- AngularUiRouter/app/categories/categories.js
+++ AngularUiRouter/app/categories/categories.js
@@ -10,6 +10,8 @@
 				url: '/',
 				// this state cotains these two views
 				views: {
+					// the '@' marks it as an absolute path
+					// meaning it targets this named view with any unamend state
 					'categories@': {
 						controller: 'CategoriesCtrl',
 						templateUrl: 'app/categories/categories.tmpl.html'
